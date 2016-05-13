440ab84aaf406d9ce74aa76430d6a85e013b8b63
Cho Han Cheol <chcbaram@paran.com>
2016-05-13 09:17:25 +0900

    OpenCR 부트로더 기본 프로젝트 추가

---
+++ arduino/opencr_bootloader/main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+void main_init();
+
+
+
+
+int main(void)
+{
+
+  main_init();
+
+
+	while (1)
+	{
+		HAL_Delay(1000);
+	}
+}
+
+
+void main_init()
+{
+
+}
---
+++ arduino/opencr_bootloader/main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stdio.h"
+#include "stm32f7xx_hal.h"
+
+
+
+
+#endif 
+
+
