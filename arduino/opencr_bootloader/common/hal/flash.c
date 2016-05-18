bf01b2057ac09879bbd415b9f35182660075caa1
idonno2000 <idonno2000>
2016-05-19 01:33:26 +0900

    flash 읽기/쓰기/지우기 기능 추가

--- arduino/opencr_bootloader/common/hal/flash.c
+++ arduino/opencr_bootloader/common/hal/flash.c
@@ -1,67 +1,131 @@
-/*
- *  flash.h
- *
- *  Created on: 2016. 5. 14.
- *      Author: Baram, PBPH
- */
-
 #include "flash.h"
 
 
+#define HW_FLASH_SECTOR0_START_ADDRESS 	((uint32_t)0x08000000)
+#define HW_FLASH_SECTOR1_START_ADDRESS 	((uint32_t)0x08008000)
+#define HW_FLASH_SECTOR2_START_ADDRESS 	((uint32_t)0x08010000)
+#define HW_FLASH_SECTOR3_START_ADDRESS 	((uint32_t)0x08018000)
+#define HW_FLASH_SECTOR4_START_ADDRESS 	((uint32_t)0x08020000)
+#define HW_FLASH_SECTOR5_START_ADDRESS 	((uint32_t)0x08040000)
+#define HW_FLASH_SECTOR6_START_ADDRESS 	((uint32_t)0x08080000)
+#define HW_FLASH_SECTOR7_START_ADDRESS 	((uint32_t)0x080C0000)
 
+#define HW_FLASH_SECTOR0_SIZE    	((uint16_t)0x00007FFF)
+#define HW_FLASH_SECTOR1_SIZE    	((uint16_t)0x00007FFF)
+#define HW_FLASH_SECTOR2_SIZE    	((uint16_t)0x00007FFF)
+#define HW_FLASH_SECTOR3_SIZE    	((uint16_t)0x00007FFF)
+#define HW_FLASH_SECTOR4_SIZE    	((uint16_t)0x0001FFFF)
+#define HW_FLASH_SECTOR5_SIZE    	((uint16_t)0x0003FFFF)
+#define HW_FLASH_SECTOR6_SIZE    	((uint16_t)0x0003FFFF)
+#define HW_FLASH_SECTOR7_SIZE    	((uint16_t)0x0003FFFF)
+
+#define HW_FLASH_WRITE_BUFSIZE			256
 
 
 void flash_init()
 {
+	//flash_erase_whole_sectors();
+}
 
+uint8_t flash_erase_whole_sectors(void)
+{
+	FLASH_StatusTypeDef err_code = FLASH_OK;
+	FLASH_EraseInitTypeDef pEraseInit;
+	uint32_t SectorError;
+
+	//except user bootloader sectors
+	pEraseInit.TypeErase = FLASH_TYPEERASE_SECTORS;
+	pEraseInit.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+	pEraseInit.Sector = FLASH_SECTOR_2;
+	pEraseInit.NbSectors = FLASH_SECTOR_TOTAL - FLASH_SECTOR_2;
+
+	HAL_FLASH_Unlock();
+
+	HAL_FLASHEx_Erase(&pEraseInit, &SectorError);
+	if(SectorError != 0xFFFFFFFF)
+	{
+		err_code = FLASH_ERROR;
+	}
+
+	HAL_FLASH_Lock();
+	return err_code;
+}
+
+uint8_t flash_erase_sector(uint32_t sector)
+{
+	FLASH_StatusTypeDef err_code = FLASH_OK;
+	FLASH_EraseInitTypeDef pEraseInit;
+	uint32_t SectorError;
+
+	pEraseInit.TypeErase = FLASH_TYPEERASE_SECTORS;
+	pEraseInit.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+	pEraseInit.Sector = sector;
+	pEraseInit.NbSectors = 1;
+
+	HAL_FLASH_Unlock();
+
+	HAL_FLASHEx_Erase(&pEraseInit, &SectorError);
+	if(SectorError != 0xFFFFFFFF) err_code = FLASH_ERROR;
+
+	HAL_FLASH_Lock();
+	return err_code;
 }
 
 
-uint8_t flash_write(uint32_t addr, uint8_t *p_data, uint32_t length, uint32_t timeout )
+uint8_t flash_write(uint32_t addr, uint8_t *p_data, uint32_t length )
 {
-  uint8_t err_code = FLASH_OK;
-  uint32_t t_time;
 
-  // TODO : flash 메모리에서 원하는 크기만큼 데이터를 Write한다.(주소 정렬은 1바이트)
-  // addr : write 주소
-  // p_data : 쓸려고하는 데이터를 넘겨줄 데이터 포인터
-  // time_out : 타임아웃 정보이며 해당 시간을 초과하면 에러 발생시킴, 단위는 ms
-  // 아래는 타임아웃을 체크하는 방법
+  //PBHP_160514
+	HAL_StatusTypeDef HAL_FLASHStatus = HAL_OK;
+	FLASH_StatusTypeDef err_code = FLASH_OK;
+	uint32_t t_time;
 
-  t_time = millis();
-  while(1)
-  {
-    if( millis()-t_time > timeout )
-    {
-      err_code = FLASH_ERR_TIMEOUT;
-    }
-  }
+	uint32_t StartAddress = addr;
+	uint32_t WriteSize;
+	uint32_t WriteData;
+	uint32_t i;
+	uint32_t DataIndex;
 
+	WriteSize = length / 4;	// 32Bit占쏙옙 占쏙옙占쏙옙
 
+	if( (WriteSize%4) > 0 ) WriteSize++;
+
+	DataIndex = 0;
+	HAL_FLASH_Unlock();
+	for( i=0; i<WriteSize; i++ )
+	{
+		WriteData  = p_data[ DataIndex++ ] << 0;
+		WriteData |= p_data[ DataIndex++ ] << 8;
+		WriteData |= p_data[ DataIndex++ ] << 16;
+		WriteData |= p_data[ DataIndex++ ] << 24;
+
+		HAL_FLASHStatus = HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, StartAddress+i*4, (uint64_t)WriteData);
+
+		if( HAL_FLASHStatus != FLASH_OK )
+		{
+		  	err_code = HAL_FLASHStatus;
+			break;
+		}
+	}
+	HAL_FLASH_Lock();
   return err_code;
 }
 
-uint8_t flash_read(uint32_t addr, uint8_t *p_data, uint32_t length, uint32_t timeout )
+
+uint8_t flash_read(uint32_t addr, uint8_t *p_data, uint32_t length)
 {
   uint8_t err_code = FLASH_OK;
-  uint32_t t_time;
 
-  // TODO : flash 메모리에서 원하는 크기만큼 데이터를 Read한다.(주소 정렬은 1바이트)
-  // addr : 읽을 주소
-  // p_data : 읽은 데이터를 넘겨줄 데이터 포인터
-  // time_out : 타임아웃 정보이며 해당 시간을 초과하면 에러 발생시킴, 단위는 ms
+	uint32_t Dataindex;
+	uint32_t addr_cnt;
 
-  t_time = millis();
-  while(1)
-  {
-    if( millis()-t_time > timeout )
-    {
-      err_code = FLASH_ERR_TIMEOUT;
-    }
-  }
-
-
-  return err_code;
+	Dataindex = 0;
+	for (addr_cnt=0;addr_cnt<length;addr_cnt++)
+	{
+		p_data[Dataindex++] = *(__IO uint8_t*)(addr+addr_cnt);
+	}
+	return err_code;
 }
+
 
 
