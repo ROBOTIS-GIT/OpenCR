085b210e2d46f1c9773a48b79d4a4392a986e124
HanCheol Cho <chcbaram@paran.com>
2016-10-05 02:52:31 +0900

    - Fixed arduino examples - Fixed pin map for OpenCR TP2 Board

--- arduino/opencr_arduino/opencr/variants/OpenCR/chip.h
+++ arduino/opencr_arduino/opencr/variants/OpenCR/chip.h
@@ -16,17 +16,49 @@
 #define USE_SPI2
 
 
-#define BOARD_NR_I2C 2
-#define HAL_I2C1 I2C1
-#define HAL_I2C2 I2C2
-#define HAL_I2C3 I2C3
+#define BOARD_NR_I2C  2
+#define HAL_I2C1      I2C1
+#define HAL_I2C2      I2C2
+#define HAL_I2C3      I2C3
 
 
-#define BOARD_NR_ADC_PINS   5
-#define BOARD_NR_PWM_PINS   12
+#define BOARD_NR_ADC_PINS       5
+#define BOARD_NR_PWM_PINS       12
 
 
-#define BDPIN_SPI_CS_IMU    28
+#define BDPIN_LED_USER_1        22
+#define BDPIN_LED_USER_2        23
+#define BDPIN_LED_USER_3        24
+#define BDPIN_LED_USER_4        25
+#define BDPIN_DIP_SW_1          26
+#define BDPIN_DIP_SW_2          27
+#define BDPIN_SPI_CS_IMU        28
+#define BDPIN_BAT_PWR_ADC       29
+#define BDPIN_CHG_PWR_ADC       30
+#define BDPIN_BUZZER            31
+#define BDPIN_DXL_PWR_EN        32
+#define BDPIN_LED_BATT_CHARGE   33
+#define BDPIN_PUSH_SW_1         34
+#define BDPIN_PUSH_SW_2         35
+
+#define BDPIN_GPIO_1            50
+#define BDPIN_GPIO_2            51
+#define BDPIN_GPIO_3            52
+#define BDPIN_GPIO_4            53
+#define BDPIN_GPIO_5            54
+#define BDPIN_GPIO_6            55
+#define BDPIN_GPIO_7            56
+#define BDPIN_GPIO_8            57
+#define BDPIN_GPIO_9            58
+#define BDPIN_GPIO_10           59
+#define BDPIN_GPIO_11           60
+#define BDPIN_GPIO_12           61
+#define BDPIN_GPIO_13           62
+#define BDPIN_GPIO_14           63
+#define BDPIN_GPIO_15           64
+#define BDPIN_GPIO_16           65
+#define BDPIN_GPIO_17           66
+#define BDPIN_GPIO_18           67
 
 
 #endif
