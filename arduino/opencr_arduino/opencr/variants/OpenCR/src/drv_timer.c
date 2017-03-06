c9e983a22169b2882da025c0fe72ad225b5d3656
HanCheol Cho <chc@robotis.com>
2017-03-06 21:03:36 +0900

    fixed usb transmit bug.

--- arduino/opencr_arduino/opencr/variants/OpenCR/src/drv_timer.c
+++ arduino/opencr_arduino/opencr/variants/OpenCR/src/drv_timer.c
@@ -156,7 +156,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim)
   if( htim->Instance == TIM6 )
   {
     __HAL_RCC_TIM6_CLK_ENABLE();
-    HAL_NVIC_SetPriority(TIM6_DAC_IRQn, 5, 0);
+    HAL_NVIC_SetPriority(TIM6_DAC_IRQn, 10, 0);
     HAL_NVIC_EnableIRQ(TIM6_DAC_IRQn);
   }
 
