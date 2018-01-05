87c3353b4b5b892b614bbb4e031ee3393f5fa7dc
OpusK <kkw@robotis.com>
2018-01-05 11:28:52 +0900

    Added CAN Examples ( four )

---
+++ arduino/opencr_arduino/opencr/libraries/OpenCR/examples/10. Etc/CAN/canTxRxMessage/canTxRxMessage.ino
@@ -0,0 +1,98 @@
+/*******************************************************************************
+* Copyright (c) 2016, ROBOTIS CO., LTD.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* * Redistributions of source code must retain the above copyright notice, this
+*   list of conditions and the following disclaimer.
+*
+* * Redistributions in binary form must reproduce the above copyright notice,
+*   this list of conditions and the following disclaimer in the documentation
+*   and/or other materials provided with the distribution.
+*
+* * Neither the name of ROBOTIS nor the names of its
+*   contributors may be used to endorse or promote products derived from
+*   this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/* Author: OpusK */
+
+
+#define SEND_INTERVAL_MS 1000
+
+uint32_t t_time, id, i;
+can_msg_t tx_msg, rx_msg;
+/*
+ *  typedef struct 
+ *  {
+ *    uint32_t id      : Identifier of received message
+ *    uint32_t length  : Length of received message data
+ *    uint8_t  data[8] : Data of received message
+ *  } can_msg_t;
+*/
+
+void setup()
+{
+  Serial.begin(115200);
+
+  Serial.println("============================");
+  Serial.println("=== CAN Message Example! ===");
+
+  if (canOpen(_DEF_CAN_BAUD_125K, _DEF_CAN_EXT) == false)
+  {
+    Serial.println("CAN open fail!!");
+  }
+  else
+  {
+    id = 0x123;
+    canConfigFilter(id, 0);
+
+    tx_msg.id = id;
+    tx_msg.length = 8;
+  }
+  t_time = millis();
+}
+
+void loop()
+{
+  if (canAvailableMsg())
+  {
+    if(canReadMsg(&rx_msg))
+    {
+      Serial.print("ID : ");
+      Serial.print(rx_msg.id, HEX);
+      Serial.print(", Length : ");
+      Serial.print(rx_msg.length);
+      Serial.print(", Data : ");
+      for (i = 0; i < rx_msg.length; i++)
+      {
+        Serial.print(rx_msg.data[i], HEX);
+        Serial.print(" ");
+      }
+      Serial.println();
+    }
+  }
+
+  if (millis() - t_time >= SEND_INTERVAL_MS)
+  {
+    t_time = millis();
+    for(i = 0; i < 8; i++)
+    {
+      tx_msg.data[i] = (uint8_t)micros() + i;
+    }
+    canWriteMsg(&tx_msg);
+  }
+}
