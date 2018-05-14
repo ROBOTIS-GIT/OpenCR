3ba5d3f93d25e1af70745350eb9c26753e16cd3a
Darby Lim <thlim@robotis.com>
2018-05-14 15:38:06 +0900

    add sensors

--- arduino/opencr_arduino/opencr/libraries/turtlebot3_ros_lib/turtlebot3_msgs/SensorState.h
+++ arduino/opencr_arduino/opencr/libraries/turtlebot3_ros_lib/turtlebot3_msgs/SensorState.h
@@ -17,8 +17,14 @@ namespace turtlebot3_msgs
       _header_type header;
       typedef uint8_t _bumper_type;
       _bumper_type bumper;
-      typedef uint8_t _cliff_type;
+      typedef float _cliff_type;
       _cliff_type cliff;
+      typedef float _sonar_type;
+      _sonar_type sonar;
+      typedef float _illumination_type;
+      _illumination_type illumination;
+      typedef uint8_t _led_type;
+      _led_type led;
       typedef uint8_t _button_type;
       _button_type button;
       typedef bool _torque_type;
@@ -29,15 +35,13 @@ namespace turtlebot3_msgs
       _right_encoder_type right_encoder;
       typedef float _battery_type;
       _battery_type battery;
-      enum { BUMPER_RIGHT =  1 };
-      enum { BUMPER_CENTER =  2 };
-      enum { BUMPER_LEFT =  4 };
-      enum { CLIFF_RIGHT =  1 };
-      enum { CLIFF_CENTER =  2 };
-      enum { CLIFF_LEFT =  4 };
+      enum { BUMPER_FORWARD =  1 };
+      enum { BUMPER_BACKWARD =  2 };
+      enum { CLIFF =  1 };
+      enum { SONAR =  1 };
+      enum { ILLUMINATION =  1 };
       enum { BUTTON0 =  1 };
       enum { BUTTON1 =  2 };
-      enum { BUTTON2 =  4 };
       enum { ERROR_LEFT_MOTOR =  1 };
       enum { ERROR_RIGHT_MOTOR =  2 };
       enum { TORQUE_ON =  1 };
@@ -47,6 +51,9 @@ namespace turtlebot3_msgs
       header(),
       bumper(0),
       cliff(0),
+      sonar(0),
+      illumination(0),
+      led(0),
       button(0),
       torque(0),
       left_encoder(0),
@@ -61,8 +68,38 @@ namespace turtlebot3_msgs
       offset += this->header.serialize(outbuffer + offset);
       *(outbuffer + offset + 0) = (this->bumper >> (8 * 0)) & 0xFF;
       offset += sizeof(this->bumper);
-      *(outbuffer + offset + 0) = (this->cliff >> (8 * 0)) & 0xFF;
+      union {
+        float real;
+        uint32_t base;
+      } u_cliff;
+      u_cliff.real = this->cliff;
+      *(outbuffer + offset + 0) = (u_cliff.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_cliff.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_cliff.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_cliff.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->cliff);
+      union {
+        float real;
+        uint32_t base;
+      } u_sonar;
+      u_sonar.real = this->sonar;
+      *(outbuffer + offset + 0) = (u_sonar.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_sonar.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_sonar.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_sonar.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->sonar);
+      union {
+        float real;
+        uint32_t base;
+      } u_illumination;
+      u_illumination.real = this->illumination;
+      *(outbuffer + offset + 0) = (u_illumination.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_illumination.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_illumination.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_illumination.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->illumination);
+      *(outbuffer + offset + 0) = (this->led >> (8 * 0)) & 0xFF;
+      offset += sizeof(this->led);
       *(outbuffer + offset + 0) = (this->button >> (8 * 0)) & 0xFF;
       offset += sizeof(this->button);
       union {
@@ -111,8 +148,41 @@ namespace turtlebot3_msgs
       offset += this->header.deserialize(inbuffer + offset);
       this->bumper =  ((uint8_t) (*(inbuffer + offset)));
       offset += sizeof(this->bumper);
-      this->cliff =  ((uint8_t) (*(inbuffer + offset)));
+      union {
+        float real;
+        uint32_t base;
+      } u_cliff;
+      u_cliff.base = 0;
+      u_cliff.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_cliff.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_cliff.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_cliff.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->cliff = u_cliff.real;
       offset += sizeof(this->cliff);
+      union {
+        float real;
+        uint32_t base;
+      } u_sonar;
+      u_sonar.base = 0;
+      u_sonar.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_sonar.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_sonar.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_sonar.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->sonar = u_sonar.real;
+      offset += sizeof(this->sonar);
+      union {
+        float real;
+        uint32_t base;
+      } u_illumination;
+      u_illumination.base = 0;
+      u_illumination.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_illumination.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_illumination.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_illumination.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->illumination = u_illumination.real;
+      offset += sizeof(this->illumination);
+      this->led =  ((uint8_t) (*(inbuffer + offset)));
+      offset += sizeof(this->led);
       this->button =  ((uint8_t) (*(inbuffer + offset)));
       offset += sizeof(this->button);
       union {
@@ -160,7 +230,7 @@ namespace turtlebot3_msgs
     }
 
     const char * getType(){ return "turtlebot3_msgs/SensorState"; };
-    const char * getMD5(){ return "d537ed7b8d95065b6c83830430b93911"; };
+    const char * getMD5(){ return "7250c1dc0b61c4190e78f528f599285f"; };
 
   };
 
