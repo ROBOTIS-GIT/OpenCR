e787ec04358b119fef3ffc0bf2bb5aed9efe711c
Darby Lim <thlim@robotis.com>
2018-06-26 10:56:45 +0900

    update version to 1.2.1

--- arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_waffle/turtlebot3_core/turtlebot3_core_config.h
+++ arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_waffle/turtlebot3_core/turtlebot3_core_config.h
@@ -45,7 +45,7 @@
 
 #define HARDWARE_VER "1.0.0"
 #define SOFTWARE_VER "1.0.0"
-#define FIRMWARE_VER "1.2.0"
+#define FIRMWARE_VER "1.2.1"
 
 #define CONTROL_MOTOR_SPEED_FREQUENCY          30   //hz
 #define IMU_PUBLISH_FREQUENCY                  200  //hz
