f16639066cc349c268682e8517a7f79cdbe36151
Darby Lim <thlim@robotis.com>
2018-08-21 15:48:48 +0900

    update function

--- arduino/opencr_arduino/opencr/libraries/OpenManipulator/example/Arduino/Chain/d_open_manipulator_chain/RemoteController.h
+++ arduino/opencr_arduino/opencr/libraries/OpenManipulator/example/Arduino/Chain/d_open_manipulator_chain/RemoteController.h
@@ -29,81 +29,81 @@ void connectRC100()
   rc100.begin(1);
 }
 
-void fromRC100()
+int availableRC100()
 {
-  if (rc100.available())
+  return rc100.available();
+}
+
+void fromRC100(uint16_t data)
+{
+  if (data & RC100_BTN_U)
+    manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(0.010, 0.0, 0.0), 0.2f);
+  else if (data & RC100_BTN_D)
+    manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(-0.010, 0.0, 0.0), 0.2f);
+  else if (data & RC100_BTN_L)
+    manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(0.0, 0.010, 0.0), 0.2f);
+  else if (data & RC100_BTN_R)
+    manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(0.0, -0.010, 0.0), 0.2f);
+  else if (data & RC100_BTN_1)
+    manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(0.0, 0.0, 0.010), 0.2f);
+  else if (data & RC100_BTN_3)
+    manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(0.0, 0.0, -0.010), 0.2f);
+  else if (data & RC100_BTN_2)
   {
-    uint16_t data = rc100.readData();
-
-    if (data & RC100_BTN_U)
-      manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(0.01, 0.0, 0.0), 0.1f);
-    else if (data & RC100_BTN_D)
-      manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(-0.01, 0.0, 0.0), 0.1f);
-    else if (data & RC100_BTN_L)
-      manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(0.0, 0.01, 0.0), 0.1f);
-    else if (data & RC100_BTN_R)
-      manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(0.0, -0.01, 0.0), 0.1f);
-    else if (data & RC100_BTN_1)
-      manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(0.0, 0.0, 0.01), 0.1f);
-    else if (data & RC100_BTN_3)
-      manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(0.0, 0.0, -0.01), 0.1f);
-    else if (data & RC100_BTN_2)
-    {
-      float grip_pose = manipulator.getComponentToolValue(CHAIN, TOOL) + 0.010;
-      manipulator.toolMove(CHAIN, TOOL, OM_MATH::map(grip_pose, 0.020f, 0.070f, 0.907f, -1.13f));
-    }
-    else if (data & RC100_BTN_4)
-    {
-      float grip_pose = manipulator.getComponentToolValue(CHAIN, TOOL) - 0.010;
-      manipulator.toolMove(CHAIN, TOOL, OM_MATH::map(grip_pose, 0.020f, 0.070f, 0.907f, -1.13f));
-    }
-    else
-      manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(0.0, 0.0, 0.0), 0.1f);
-
-    // else if (receive_data & RC100_BTN_5)
-    // {
-    //   // target_pos[1] = 0.0;
-    //   // target_pos[2] = 60.0  * DEG2RAD;
-    //   // target_pos[3] = -20.0 * DEG2RAD;
-    //   // target_pos[4] = -40.0 * DEG2RAD;
-
-    //   // setJointAngle(target_pos);
-    //   // move();
-
-    //   motion_cnt = 0;
-    //   motion     = false;
-    //   repeat     = false;
-    // }
-    // else if (receive_data & RC100_BTN_6)
-    // {
-    //   // target_pos[1] = 0.0;
-    //   // target_pos[2] = 0.0;
-    //   // target_pos[3] = 0.0;
-    //   // target_pos[4] = 0.0;
-
-    //   // setJointAngle(target_pos);
-    //   // move();
-
-    //   if (DYNAMIXEL)
-    //       sendAngle2Processing(getAngle());
-
-    //   if (PROCESSING)
-    //     sendAngle2Processing(getState());
-
-    //   for (int i = 0; i < MOTION_NUM; i++)
-    //   {
-    //     for (int j = 0; j < 6; j++)
-    //     {
-    //       motion_storage[i][j] = motion_set[i][j];
-    //     }
-    //   }
-
-    //   motion_num = MOTION_NUM;
-    //   motion_cnt = 0;
-    //   motion = true;
-    //   repeat = true;
-    // }
+    float grip_pose = manipulator.getComponentToolValue(CHAIN, TOOL) + 0.010;
+    manipulator.toolMove(CHAIN, TOOL, OM_MATH::map(grip_pose, 0.020f, 0.070f, 0.907f, -1.13f));
   }
+  else if (data & RC100_BTN_4)
+  {
+    float grip_pose = manipulator.getComponentToolValue(CHAIN, TOOL) - 0.010;
+    manipulator.toolMove(CHAIN, TOOL, OM_MATH::map(grip_pose, 0.020f, 0.070f, 0.907f, -1.13f));
+  }
+  else
+    manipulator.setMove(CHAIN, TOOL, OM_MATH::makeVector3(0.0, 0.0, 0.0), 0.1f);
+
+  // else if (receive_data & RC100_BTN_5)
+  // {
+  //   // target_pos[1] = 0.0;
+  //   // target_pos[2] = 60.0  * DEG2RAD;
+  //   // target_pos[3] = -20.0 * DEG2RAD;
+  //   // target_pos[4] = -40.0 * DEG2RAD;
+
+  //   // setJointAngle(target_pos);
+  //   // move();
+
+  //   motion_cnt = 0;
+  //   motion     = false;
+  //   repeat     = false;
+  // }
+  // else if (receive_data & RC100_BTN_6)
+  // {
+  //   // target_pos[1] = 0.0;
+  //   // target_pos[2] = 0.0;
+  //   // target_pos[3] = 0.0;
+  //   // target_pos[4] = 0.0;
+
+  //   // setJointAngle(target_pos);
+  //   // move();
+
+  //   if (DYNAMIXEL)
+  //       sendAngle2Processing(getAngle());
+
+  //   if (PROCESSING)
+  //     sendAngle2Processing(getState());
+
+  //   for (int i = 0; i < MOTION_NUM; i++)
+  //   {
+  //     for (int j = 0; j < 6; j++)
+  //     {
+  //       motion_storage[i][j] = motion_set[i][j];
+  //     }
+  //   }
+
+  //   motion_num = MOTION_NUM;
+  //   motion_cnt = 0;
+  //   motion = true;
+  //   repeat = true;
+  // }
 }
 
 #endif
